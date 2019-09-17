dc3bad231101c20dda94839c527332191b951dc0
k00lagin <k00lagin@gmail.com>
2019-09-17 14:40:38 +0500

    Отрисовка статистики

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,8 @@
 'use strict';
 window.renderStatistics = function (ctx, names, times) {
+  var MAX_BAR_HEIGHT = 150;
+  var BAR_WIDTH = 40;
+
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = '#FFFFFF';
@@ -9,8 +12,28 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
+  var maxTime = times.reduce(function(previousTime, currentTime) {
+    if (currentTime > previousTime) {
+      return currentTime;
+    } else {
+      return previousTime;
+    }
+  }, 0);
+  for (var playerIndex = 0; playerIndex < names.length; playerIndex++) {
+    var playerName = names[playerIndex];
+    var playerTime = times[playerIndex];
+    var currentBarHeight = playerTime * MAX_BAR_HEIGHT / maxTime;
+
+    if (playerName === 'Вы') {
+      ctx.fillStyle = 'red';
+    } else {
+      ctx.fillStyle = `hsl(230, ${Math.random() * 100}%, 50%)`;
+    }
+    ctx.fillRect(140 + playerIndex * 90, 90 + (MAX_BAR_HEIGHT - currentBarHeight), BAR_WIDTH, currentBarHeight);
+
+    ctx.fillStyle = '#000000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText(Math.floor(playerTime), 140 + playerIndex * 90, 80 + (MAX_BAR_HEIGHT - currentBarHeight));
+    ctx.fillText(playerName, 140 + playerIndex * 90, 110 + MAX_BAR_HEIGHT);
+  }
 }
-
-// renderBar function(value, title, color, x) {
-
-// }
