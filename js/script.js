6c4afed7a0ddc08255a0d9420c2ddc40f67f09aa
Vsevolod Kulagin <k00lagin@gmail.com>
2019-09-21 15:13:44 +0500

    Откат самодеятельности

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 'use strict';
 var WIZARD_PROPORTION = 1.337;
-var WIND_SPEED = 1.5;
-var FIREBALL_BASE_SPEED = 3.5;
 
 var fireballSize = 22;
 var wizardSpeed = 3;
 var wizardWidth = 70;
 
-
 function getFireballSpeed(left) {
-  if (left) {
-    return FIREBALL_BASE_SPEED + WIND_SPEED;
-  } else {
-    return FIREBALL_BASE_SPEED - WIND_SPEED;
-  }
+  return left ? 5 : 2;
 }
 
 function getWizardHeight() {
